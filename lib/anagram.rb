34ff6fce830d488b3dad584b1f991ef48dd652d5
Allison Ricker <alliricker@gmail.com>
2020-10-11 20:31:45 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,10 +2,15 @@ class Anagram
 
   attr_accessor :word
 
+@@all = []
+
   def intialize(word)
     @word = word
+    @@all << self 
   end
 
-
+def self.match
+  @all.map { |x| x == word }
+end
 
 end
