c2c9528d2fd218b256256b17a7d799c46a667b33
Allison Ricker <alliricker@gmail.com>
2020-10-11 20:43:05 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,16 +2,13 @@ class Anagram
 
   attr_accessor :word
 
-
   def initialize(word)
     @word = word
 
   end
 
-def match(words)
-  words.select { |x|
-     (@word.split("").sort) == (x.split("").sort) }
-end
-
+  def match(words)
+    words.select { |x| (@word.split("").sort) == (x.split("").sort) }
+  end
 
 end
